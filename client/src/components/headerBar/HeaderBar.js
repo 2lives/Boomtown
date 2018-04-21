1a8c3c14e951ba5ebd2a98a65e5a53f73a2ad5d3
Brandon Liu <brandon.slipjet@gmail.com>
2018-04-21 09:51:45 -0700

    changed eslint settings to accept more spaces in tabd, and added appbar with removed hamburger menu

--- client/src/components/headerBar/HeaderBar.js
+++ client/src/components/headerBar/HeaderBar.js
@@ -1,91 +1,101 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import RaisedButton from "material-ui/RaisedButton";
-import { Link, Route } from "react-router-dom";
-import logo from "../../images/boomtown-logo.svg";
-import AppBar from "material-ui/AppBar";
-import FlatButton from "material-ui/FlatButton";
-import { grey900, grey500 } from "material-ui/styles/colors";
-import TagFilterField from "../TagFilterField";
-import { connect } from "react-redux";
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import RaisedButton from 'material-ui/RaisedButton';
+import { Link, Route } from 'react-router-dom';
+import logo from '../../images/boomtown-logo.svg';
+import AppBar from 'material-ui/AppBar';
+import FlatButton from 'material-ui/FlatButton';
+import { grey900, grey500 } from 'material-ui/styles/colors';
+import TagFilterField from '../TagFilterField';
+import { connect } from 'react-redux';
 import {
-  get_items_and_users,
-  get_items_filter
-} from "../../redux/modules/Items";
+    get_items_and_users,
+    get_items_filter
+} from '../../redux/modules/Items';
 
 const styles = {
-  width: "100vw",
-  padding: "10px",
-  marginBottom: "50px",
-  display: "flex",
-  justifyContent: "space-between",
-  profileButton: {
-    color: "white"
-  },
-  logoutButton: {
-    color: "white",
-    margin: "0 20px"
-  },
-  image: {
-    width: "35px",
-    height: "35px"
-  }
+    width: '100vw',
+    padding: '10px',
+    marginBottom: '50px',
+    display: 'flex',
+    justifyContent: 'space-between',
+    profileButton: {
+        color: 'white'
+    },
+    logoutButton: {
+        color: 'white',
+        margin: '0 20px'
+    },
+    image: {
+        position: 'absolute',
+        left: '0',
+        width: '35px',
+        height: '35px'
+    },
+    TagFilterField: {
+        position: 'absolute',
+        left: '0',
+        marginLeft: '20px'
+    }
 };
 
 class HeaderBar extends Component {
-  componentDidMount() {
-    const urls = ["http://localhost:3000/items", "http://localhost:3000/users"];
-    this.props.dispatch(get_items_and_users(urls));
-  }
-
-  getTags = items => {
-    let tags = [];
-    if (items.length && items[0] !== undefined) {
-      items.map(item => {
-        if (item.tags !== undefined) {
-          if (!item.tags.includes(undefined)) {
-            item.tags.map(tag => {
-              if (!tags.includes(tag)) {
-                tags.push(tag);
-              }
-            });
-          }
-        }
-      });
+    componentDidMount() {
+        const urls = [
+            'http://localhost:3000/items',
+            'http://localhost:3000/users'
+        ];
+        this.props.dispatch(get_items_and_users(urls));
     }
-    return tags;
-  };
-  render() {
-    const tags = this.getTags(this.props.itemsData.items);
-    console.log(tags);
-    return (
-      <AppBar style={styles}>
-        <Link to={"/"}>
-          <img src={logo} alt="Boomtown Logo" style={styles.image} />
-        </Link>
-        {tags.length && <TagFilterField tags={tags} />}
-        <div>
-          <RaisedButton
-            label="My Profile"
-            primary={true}
-            style={styles.profileButton}
-          />
 
-          <RaisedButton
-            label="Logout"
-            secondary={true}
-            style={styles.logoutButton}
-          />
-        </div>
-      </AppBar>
-    );
-  }
+    getTags = items => {
+        let tags = [];
+        if (items.length && items[0] !== undefined) {
+            items.map(item => {
+                if (item.tags !== undefined) {
+                    if (!item.tags.includes(undefined)) {
+                        item.tags.map(tag => {
+                            if (!tags.includes(tag)) {
+                                tags.push(tag);
+                            }
+                        });
+                    }
+                }
+            });
+        }
+        return tags;
+    };
+    render() {
+        const tags = this.getTags(this.props.itemsData.items);
+        console.log(tags);
+        return (
+            <AppBar style={styles} showMenuIconButton={false}>
+                <Link to={'/'}>
+                    <img src={logo} alt="Boomtown Logo" style={styles.image} />
+                </Link>
+                {tags.length && <TagFilterField tags={tags} style={styles} />}
+                <div>
+                    <RaisedButton
+                        label="My Profile"
+                        primary={true}
+                        style={styles.profileButton}
+                    />
+
+                    <RaisedButton
+                        label="Logout"
+                        secondary={true}
+                        style={styles.logoutButton}
+                    />
+                </div>
+            </AppBar>
+        );
+    }
 }
 
 const mapStateToProps = state => ({
-  isLoading: state.itemsData.isLoading,
-  itemsData: state.itemsData,
-  itemFilters: state.itemsData.itemFilters
+    isLoading: state.itemsData.isLoading,
+    itemsData: state.itemsData,
+    itemFilters: state.itemsData.itemFilters
 });
 
 export default connect(mapStateToProps)(HeaderBar);
--- client/src/containers/Profile/ProfileContainer.js
+++ client/src/containers/Profile/ProfileContainer.js
@@ -1,47 +1,52 @@
-import React, { Component } from "react";
-import Profile from "./Profile";
-import ItemCardList from "../../components/itemCardList";
+import React, { Component } from 'react';
+import Profile from './Profile';
+import ItemCardList from '../../components/itemCardList';
 
 class ProfileContainer extends Component {
-  constructor() {
-    super();
-    this.state = {
-      isLoading: false,
-      itemsData: []
-    };
-  }
+    constructor() {
+        super();
+        this.state = {
+            isLoading: false,
+            itemsData: []
+        };
+    }
 
-  componentDidMount() {
-    const urls = ["http://localhost:3000/items", "http://localhost:3000/users"];
-    this.setState({ isLoading: true });
-    Promise.all(urls.map(url => fetch(url).then(resp => resp.json())))
-      .then(resp => {
-        resp[0].map(item => {
-          resp[1].map(user => {
-            if (user.id === item.itemowner) {
-              item.itemowner = user;
-            }
-          });
-        });
-        this.setState({ itemsData: resp[0] });
-        let personalItems = this.state.itemsData.filter(
-          item => item.itemowner.id === this.props.match.params.itemownerId
+    componentDidMount() {
+        const urls = [
+            'http://localhost:3000/items',
+            'http://localhost:3000/users'
+        ];
+        this.setState({ isLoading: true });
+        Promise.all(urls.map(url => fetch(url).then(resp => resp.json())))
+            .then(resp => {
+                resp[0].map(item => {
+                    resp[1].map(user => {
+                        if (user.id === item.itemowner) {
+                            item.itemowner = user;
+                        }
+                    });
+                });
+                this.setState({ itemsData: resp[0] });
+                let personalItems = this.state.itemsData.filter(
+                    item =>
+                        item.itemowner.id ===
+                        this.props.match.params.itemownerId
+                );
+                this.setState({ itemsData: personalItems });
+            })
+            .then(() => this.setState({ isLoading: false }))
+            .catch(error => console.log(error));
+        //     const profileData = this.state.itemsData.map();
+        //     console.log(profileData);
+    }
+
+    render() {
+        return (
+            <div>
+                <Profile profileData={this.state.itemsData} />
+                <ItemCardList itemsData={this.state.itemsData} />
+            </div>
         );
-        this.setState({ itemsData: personalItems });
-      })
-      .then(() => this.setState({ isLoading: false }))
-      .catch(error => console.log(error));
-    //     const profileData = this.state.itemsData.map();
-    //     console.log(profileData);
-  }
-
-  render() {
-    return (
-      <div>
-        <Profile profileData={this.state.itemsData} />
-        <ItemCardList itemsData={this.state.itemsData} />
-      </div>
-    );
-  }
+    }
 }
 export default ProfileContainer;
